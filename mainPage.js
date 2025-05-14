266708f36230fb497b6af31f7fd04885b86f0531
Valereon0 <sedelnikov.valeri@gmail.com>
2025-05-14 22:39:26 +0300

    fixed error

--- mainPage.js
+++ mainPage.js
@@ -1,5 +1,5 @@
 const isRegistered = localStorage.getItem('isRegistered')
 
 if (!isRegistered && window.location.pathname.includes('/main.html')) {
-    window.location.href = './register.html'
+    window.location.href = './index.html'
 }
